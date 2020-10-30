14b18ee4677bd9c03c8dd050073dc95a2625534c
Roman Pogorelov <rmk140987@gmail.com>
2020-10-30 09:15:52 +0300

    4.1. Added address calculation.

--- js/page.js
+++ js/page.js
@@ -1,11 +1,38 @@
 'use strict';
 
+const mainPinData = {
+  defaultTop: 375,
+  defaultLeft: 570,
+  width: 62,
+  height: 62,
+  pinHeight: 84,
+  getY() {
+    return this.defaultTop + this.height / 2;
+  },
+  getDefaultActiveY() {
+    return this.defaultTop + this.pinHeight / 2;
+  },
+  getX() {
+    return this.defaultLeft + this.width / 2;
+  }
+};
+
 const mainPin = document.querySelector(`.map__pin--main`);
 const map = document.querySelector(`.map`);
 const adForm = document.querySelector(`.ad-form`);
 const mapFilters = document.querySelector(`.map__filters`);
 
 const mapFiltersContainer = document.querySelector(`.map__filters-container`);
+const address = document.querySelector(`#address`);
+
+let state = false;
+
+function setAddress() {
+  const y = state ? mainPinData.getDefaultActiveY() : mainPinData.getY();
+  const x = mainPinData.getX();
+
+  address.value = `${x}, ${y}`;
+}
 
 function onMainPinClick(evt) {
   const MAIN_BUTTON_CODE = 0;
@@ -14,11 +41,13 @@ function onMainPinClick(evt) {
 
   if (evt.button === MAIN_BUTTON_CODE) {
     pageActivation();
+    setAddress();
   }
 }
 
 function onMainPinEnterClick(evt) {
   evt.preventDefault();
+
   if (evt.key === `Enter`) {
     pageActivation();
   }
@@ -28,6 +57,7 @@ mainPin.addEventListener(`mousedown`, onMainPinClick);
 mainPin.addEventListener(`keydown`, onMainPinEnterClick);
 
 function pageInActivation() {
+  state = false;
   map.classList.add(`map--faded`);
   adForm.classList.add(`ad-form--disabled`);
 
@@ -37,9 +67,12 @@ function pageInActivation() {
   for (let child of mapFilters) {
     child.disabled = true;
   }
+
+  setAddress();
 }
 
 function pageActivation() {
+  state = true;
   map.classList.remove(`map--faded`);
   adForm.classList.remove(`ad-form--disabled`);
 
